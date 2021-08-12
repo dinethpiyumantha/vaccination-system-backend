ee777f624a9a399a1357a4b0642cdb8a0bb4fbaa
dinethpiyumantha <dinethpiyumantha@gmail.com>
2021-08-12 16:37:42 +0530

    all CRUD functions implemented

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -5,11 +5,67 @@ namespace App\Http\Controllers;
 use App\Models\Person;
 use Illuminate\Http\Request;
 
+/**
+ * Author : EKANAYAKA GMDP (IT19955650)
+ * all person methods implemented in PersonController class
+ */
 class PersonController extends Controller
 {
-    //
+    //Return all persons as JSON from people table
     public function getAll() {
         $people = Person::all();
         return response()->json(['results'=>$people],200);
+    }
+
+    //Return a person as JSON boject from people table by searialno
+    public function getPersonById($id) {
+        $person = Person::find($id);
+        $response = [
+            'person'=>$person
+        ];
+        return response()->json($response,200);
+    }
+
+    //Insert a person into people table
+    public function postPerson(Request $request) {
+        $person = new Person();
+        $person->serialno = $request->input('serialno');
+        $person->name = $request->input('name');
+        $person->nic = $request->input('nic');
+        $person->age = $request->input('age');
+        $person->gender = $request->input('gender');
+        $person->address = $request->input('address');
+        $person->save();
+        return response()->json(['person'=>$person, 'response'=>true], 201);
+    }
+
+    //Update a person in people table
+    public function updatePerson(Request $request, $id) {
+        $person = Person::find($id);
+        if(!$person){
+            return response()->json(['message'=>"Person not found"],404);
+        }
+        $person->serialno = $request->input('serialno');
+        $person->name = $request->input('name');
+        $person->nic = $request->input('nic');
+        $person->age = $request->input('age');
+        $person->gender = $request->input('gender');
+        $person->address = $request->input('address');
+        $person->save();
+        return response()->json(['person'=>$person],200);
+    }
+
+    //Delete a person from people table
+    public function deletePerson($id) {
+        $person = Person::find($id);
+        if(!$person) {
+            return response()->json([
+                "message"=>"Item not found !", 404 
+            ]);
+        }
+        $person->delete();
+        return response()->json([
+            "message"=>"Person `$person->id` Deleted !", 201
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\PersonController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
+/* 
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
@@ -13,16 +13,25 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 
-Route::get('/test', function() {
-    $myObj = array('results' => array('serial' => "P001", 'name' => 'Dineth Piyumantha', 'nic' => '972851990V', 'age' => 23, 'gender' => 'male', 'address' => '54/1, Maha Madagalla, Polpithigama'));
-    return $myObj;
-});
-
+/**
+ * --------------------------
+ * PersonControler Methods 
+ * --------------------------
+ * getAll()
+ * getPersonById()
+ * postPerson()
+ * updatePerson()
+ * deletePerson()
+ */
 Route::get('/person/all', [PersonController::class, 'getAll']);
+Route::get('/person/get/{id}', [PersonController::class, 'getPersonById']);
+Route::post('/person/add',[PersonController::class, 'postPerson']);
+Route::put('/person/update/{id}', [PersonController::class, 'updatePerson']);
+Route::delete('/person/delete/{id}', [PersonController::class, 'deletePerson']);
